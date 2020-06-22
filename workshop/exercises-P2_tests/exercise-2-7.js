f5b4ce3125c4cd9ca94fce0c055fd4415c5556a4
Jeffrey <jeffreyplacido@hotmail.com>
2020-06-22 17:53:17 -0400

    E2-7

--- workshop/exercises-P2_tests/exercise-2-7.js
+++ workshop/exercises-P2_tests/exercise-2-7.js
@@ -11,12 +11,39 @@
 // f(["fo", 3]) // "fofofo"
 // f(["foo", -1]) // ""
 
-function repeat(arr) {
-  // Your code here
+function repeat(arr) {  if (arr.length == 2) 
+
+{
+  return arr[1] > 0 ? arr[0].repeat(arr[1]) : ''
+} 
+
+else return undefined;
+
 }
+
+//teachers solution for future reference
+//function f(arr) {
+//const str = arr[0];
+//const count = arr[1];
+//if (typeof str !== 'string' || typeof count !== 'number') return undefined;
+//if (count <= 0) return '';
+//let res = '';
+//for (let i = 0; i < count; i++) {
+//res = res + str;
+//}
+//return res;
+
 
 // We need 7 test cases.
 // Don't forget to test all of the question parameters
+
+expect(repeat(['bacon', 3]), 'baconbaconbacon');
+expect(repeat(['bacon', 7]), 'baconbaconbaconbaconbaconbaconbacon');
+expect(repeat(['notbacon', -6]), '');
+expect(repeat(['welovebacon', 9]), 'welovebaconwelovebaconwelovebaconwelovebaconwelovebaconwelovebaconwelovebaconwelovebaconwelovebacon');
+expect(repeat(['salad', 4, 'salad']), undefined);
+expect(repeat(['pancetta or bacon', 3]), 'pancetta or baconpancetta or baconpancetta or bacon');
+expect(repeat(['bacon4', 4]), 'bacon4bacon4bacon4bacon4');
 
 /**
  * -------------------------------------------------------------------
