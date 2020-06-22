97b893034b16537c538fe9ffab6e483c4bba2e12
Jeffrey <jeffreyplacido@hotmail.com>
2020-06-22 14:19:35 -0400

    E2-4

--- workshop/exercises-P2_tests/exercise-2-4.js
+++ workshop/exercises-P2_tests/exercise-2-4.js
@@ -10,12 +10,18 @@
 // getLetterAtIndex("abc", 0);   // a
 
 function getLetterAtIndex(str, index) {
-  // Your code here
+  return str[index];
 }
 
 // Add 6 more test cases
 expect(getLetterAtIndex('hello', 4), 'o');
 expect(getLetterAtIndex('goodbye', 0), 'g');
+expect(getLetterAtIndex('bacon', 2), 'c');
+expect(getLetterAtIndex('pancetta', 6), 't');
+expect(getLetterAtIndex('Azcanta', 4), 'n');
+expect(getLetterAtIndex('', 5), undefined);
+expect(getLetterAtIndex('Thassa', 3), 's');
+expect(getLetterAtIndex('Xenagos', 40), undefined);
 
 /**
  * -------------------------------------------------------------------
