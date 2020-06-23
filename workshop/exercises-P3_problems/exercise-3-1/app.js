6975122a51f686810acf67a2c6bc7baca1b996b3
Jeffrey <jeffreyplacido@hotmail.com>
2020-06-22 21:54:06 -0400

    Q3.1

--- workshop/exercises-P3_problems/exercise-3-1/app.js
+++ workshop/exercises-P3_problems/exercise-3-1/app.js
@@ -6,3 +6,32 @@
 // Guidelines
 // - write only one event listener
 // - use a switch statement
+
+const container = document.querySelector("#btn-list");
+
+function onClick(event) {
+const btnID = event.target.id;
+console.log(btnID);
+switch (btnID) {
+    case "btn-1":
+    event.target.style.opacity = "0";
+    break;
+
+    case "btn-2":
+    event.target.style.backgroundColor = "red";
+    break;
+
+    case "btn-3":
+    event.target.style.backgroundColor = "lightblue";
+    break;
+
+    case "btn-4":
+    event.target.classList.add("jitters");
+    break;
+
+    default:
+    break;
+}
+}
+
+container.addEventListener("mousedown", onClick);
